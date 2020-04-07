b3665c3fca318f8b0fbf9763f5297f77749f40fc
changxianglin <281532612@qq.com>
2020-04-07 23:36:15 +0800

    learn to column count

---
+++ const.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define LOWER 0 /* 表的下限 */
+#define UPPER 300 /* 表的下限 */
+#define STEP 20 /* 步长 */
+
+int main() {
+	int fahr;
+	
+	for(fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}
